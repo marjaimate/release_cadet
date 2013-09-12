186a5cc79469fd31b053d74fcb5032a699cef5ea
Máté Marjai <mate.marjai@sumup.com>
2013-09-12 13:10:46 +0100

    Added feature_list as the next command.

--- lib/release_cadet.rb
+++ lib/release_cadet.rb
@@ -1,3 +1,4 @@
 require 'release_cadet/configuration'
 require 'release_cadet/command'
 require 'release_cadet/push_up'
+require 'release_cadet/feature_list'
---
+++ lib/release_cadet/feature_list.rb
@@ -0,0 +1,11 @@
+module ReleaseCadet
+  class FeatureList < Command
+    def execute release
+      branch = @config['prefixes']['release'] + release
+      puts "<RC> Checking out the target release '#{release}'"
+      `git fetch --all && git checkout #{branch}`
+      puts "<RC> Merge commits between the release and master:\n"
+      `git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr %an)%Creset' --abbrev-commit --date=relative origin/master..#{branch} --merges`
+    end
+  end
+end
