0c262efc845027025224e503239c99aa03ccba11
Máté Marjai <mate.marjai@sumup.com>
2013-09-16 15:48:48 +0100

    Adding in checks on the merge - abort if merge failed.

--- lib/release_cadet/push_up.rb
+++ lib/release_cadet/push_up.rb
@@ -4,9 +4,14 @@ module ReleaseCadet
       puts "<RC> Checking out the target branch '#{to}'"
       puts `git fetch --all && git checkout #{to}`
       puts "<RC> Merging in #{from}"
-      puts `git merge origin/#{from}`
-      puts "<RC> Pushing up to origin"
-      puts `git push origin #{to}`
+      merge_output = `git merge origin/#{from}`
+      if 0 == $?.exitstatus
+        puts merge_output
+        puts "<RC> Pushing up to origin"
+        puts `git push origin #{to}`
+      else
+        puts "<RC> Merge failed, aborting. Please check your local branch!"
+      end
     end
   end
 end
