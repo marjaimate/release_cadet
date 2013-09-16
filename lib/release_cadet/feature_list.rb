685ca6f40328ec6b8e8cfa5e3f0c3a33e275bab3
Máté Marjai <mate.marjai@sumup.com>
2013-09-16 15:44:33 +0100

    Added in output for all commands.

--- lib/release_cadet/feature_list.rb
+++ lib/release_cadet/feature_list.rb
@@ -3,9 +3,9 @@ module ReleaseCadet
     def execute release
       branch = @config['prefixes']['release'] + release
       puts "<RC> Checking out the target release '#{release}'"
-      `git fetch --all && git checkout #{branch}`
+      puts `git fetch --all && git checkout #{branch}`
       puts "<RC> Merge commits between the release and master:\n"
-      `git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr %an)%Creset' --abbrev-commit --date=relative origin/master..#{branch} --merges`
+      puts `git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr %an)%Creset' --abbrev-commit --date=relative origin/master..#{branch} --merges`
     end
   end
 end
--- lib/release_cadet/push_up.rb
+++ lib/release_cadet/push_up.rb
@@ -2,11 +2,11 @@ module ReleaseCadet
   class PushUp < Command
     def execute from, to
       puts "<RC> Checking out the target branch '#{to}'"
-      `git fetch --all && git checkout #{to}`
+      puts `git fetch --all && git checkout #{to}`
       puts "<RC> Merging in #{from}"
-      `git merge origin/#{from}`
+      puts `git merge origin/#{from}`
       puts "<RC> Pushing up to origin"
-      `git push origin #{to}`
+      puts `git push origin #{to}`
     end
   end
 end
