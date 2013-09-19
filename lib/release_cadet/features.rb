5024f08642a29ea6c2faa9aae793544ead6e4b90
Máté Marjai <mate.marjai@sumup.com>
2013-09-19 12:41:16 +0100

    Added in diff on features merged in branches.

--- lib/release_cadet/features.rb
+++ lib/release_cadet/features.rb
@@ -9,8 +9,18 @@ module ReleaseCadet
       output << fetch if is_verbose?
       output << "<RC> Merge commits between #{branch} and #{against}:" if is_verbose?
       output << `git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr %an)%Creset' --abbrev-commit --date=relative origin/#{against}..#{branch} --merges`
+      output << "<RC> Branches merged into #{branch} and not into #{against}:"
+      diff = get_merged_branches_in(branch) - get_merged_branches_in(against)
+      output << diff.join("\n")
       # output
       puts output.join("\n")
+    end
+
+    def get_merged_branches_in branch
+      fetch = fetch_and_checkout(branch)
+      `git branch -r --merged`.split("\n").map do |a| 
+        a.gsub(/\s+/, "").gsub(/origin\//,"")
+      end
     end
   end
 end
